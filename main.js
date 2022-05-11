57815520dcabf0bc03f101e328ce61ef21032706
asdas <sadasd@gmail.com>
2022-05-11 17:02:53 +0500

    Js

---
+++ main.js
@@ -0,0 +1,11 @@
+var a = +prompt('Введите первое число')
+var b = +prompt('Введите второе число')
+var c = +prompt('Введите третее число')
+
+if(a < b && a > c || a > b && a < c) {
+    alert('Среднее число ' + a)
+}else if(b < a && b > c || b > a && b < c) {
+    alert('Среднее число ' + b)
+}else if(c < a && c > b || c > a && c < b) {
+    alert('Среднее число ' + c)
+}
